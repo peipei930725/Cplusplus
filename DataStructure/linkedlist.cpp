312e5117ae2756ccc35157c2919ab8df8b3294a7
watermelon0725 <peipei930725@gmail.com>
2024-01-12 12:15:51 +0800

    ds

---
+++ DataStructure/linkedlist.cpp
@@ -0,0 +1,52 @@
+#include"linkedlist.h"
+#include<iostream>
+
+using namespace std;
+
+void LinkedList::PrintList(){
+    if(first==0){
+        cout<<"array is empty"<<endl;
+    }
+    Node *current =first;
+    while(current!=0){
+        cout<<current->data<<" ";
+        current=current->next;
+    }
+}
+void LinkedList::Push_front(int n){
+    Node *num;
+    num->next=first;
+    num->data=n;
+}
+void LinkedList::Push_back(int n){
+    Node *ptr=first;
+    for(int i=0;i<size;++i){
+        ptr=ptr->next;
+    }
+    Node*newNode;
+    ptr->next=newNode;
+    newNode->data=n;
+}
+void LinkedList::Delete(int n){
+    Node *flag=first;
+    while(first->data==n&&first!=0){
+        first=first->next;
+    }
+    flag=first;
+    while(flag->next->data==n && flag->next!=0){
+        flag->next=flag->next->next;
+    }
+}
+void LinkedList::Clear(){
+    while (first != 0) { 
+        Node *current = first;
+        first = first->next;
+        delete current;
+        current = 0;
+    }
+}
+void LinkedList::Reverse(){
+
+}
+// A->B->C->D
+// D->C
---
+++ DataStructure/linkedlist.h
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+};
+
+class LinkedList{
+private:
+    int size;                // size是用來記錄Linked list的長度, 非必要
+    int back;
+    Node *first;            // list的第一個node
+    void Reverse(Node* pre,Node* now);
+public:
+    LinkedList():first(0){};
+    void PrintList();           // 印出list的所有資料
+    void Push_front(int x);     // 在list的開頭新增node
+    void Push_back(int x);      // 在list的尾巴新增node
+    void Delete(int x);         // 刪除list中的 int x
+    void Clear();               // 把整串list刪除
+    void Reverse();              // 將list反轉: 7->3->14 => 14->3->7
+    
+
+};
