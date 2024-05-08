c78ce831c3197fc76378420b2f5ff217d1a3bfa2
watermelon0725 <peipei930725@gmail.com>
2024-05-09 00:03:00 +0800

    123

--- codeforces/943/B.cpp
+++ codeforces/943/B.cpp
@@ -1,21 +1,45 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
-    int t=0;
-    cin>>t;
-    while(t--){
-        int n,m;
-        cin>>n>>m;
-        string a;
-        string b;
-        string sub;
-        cin>>a>>b;
-        for(int i=0;i<n;++i){
-            
+int max_subsequence_length(string a, string b) {
+    int m = a.length(), n = b.length();
+    int i = 0, j = 0;
+    int max_len = 0, curr_len = 0;
+
+    while (j < n) {
+        if (j < n) {
+            if(a[i] == b[j]){
+                curr_len++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        } else {
+            max_len = max(max_len, curr_len);
+            curr_len = 0;
+            j++;
+            i = 0;  // 重置i回到字串a的開頭
         }
-        cout<<a<<b<<endl;
     }
+
+    return max(max_len, curr_len);
+}
+
+int main() {
+    int t;
+    cin >> t;  // 輸入測試案例數量
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;  // 輸入字串a和b的長度
+        string a, b;
+        cin >> a >> b;  // 輸入字串a和b
+        cout << max_subsequence_length(a, b) << endl;  // 輸出最長子序列長度
+    }
+
+    return 0;
 }
