6d42599c73be1b8b0396bea4b6053e98710bacf9
watermelon0725 <peipei930725@gmail.com>
2024-05-09 00:02:28 +0800

    111

---
+++ codeforces/943/C.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t=0;
+    cin>>t;
+    while(t--){
+        int n=0;
+        cin>>n;
+        int ans[n];
+        n--;
+        int a[n];
+        for(int i=0;i<n;++i){
+            cin>>a[i];
+        }
+        ans[0]=a[0]+1;
+        for(int i=1;i<n+1;++i){
+            if(a[i-1]>ans[i-1]){
+                i--;
+                ans[i]=a[i-1]+ans[i-1]*(a[1]/ans[i-1]+1);
+            }
+            else{ 
+                ans[i]=a[i-1]+ans[i-1];
+            }
+        }
+        for(int i=0;i<n+1;++i){
+            cout<<ans[i]<<" ";
+        }
+    }
+    return 0;
+}
